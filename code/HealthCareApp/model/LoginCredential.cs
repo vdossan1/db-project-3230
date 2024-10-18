e542221ab623c3ef4a0733fae5718479a6965021
Vitor dos Santos <vdossan1@my.westga.edu>
2024-10-18 08:33:40 -0400

    add basic login logic
    added necessary components and logic to authenticate credentials from the login page in the database. If login fails, displays error message

---
+++ code/HealthCareApp/model/LoginCredential.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareApp.model
+{
+    public class LoginCredential
+    {
+        public string Username { get; }
+        public string Password { get; }
+
+        public LoginCredential(string username, string password)
+        {
+            this.Username = username ?? throw new ArgumentNullException(nameof(username));
+            this.Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+    }
+}
---
+++ code/HealthCareApp/viewmodel/LoginPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthCareApp.DAL;
+
+namespace HealthCareApp.viewmodel
+{
+    public class LoginPageViewModel
+    {
+
+        public bool TryLogin(string username, string password)
+        {
+            return LoginCredentialDal.AuthenticateUser(username, password);
+        }
+    }
+}
