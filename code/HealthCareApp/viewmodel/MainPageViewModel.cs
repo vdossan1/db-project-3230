89963878154c67d0db7c61331f560937b17b9b77
Jacob Evans <je00059@my.westga.edu>
2024-10-24 23:56:23 -0400

    Removed Command Pattern

--- code/HealthCareApp/viewmodel/MainPageViewModel.cs
+++ code/HealthCareApp/viewmodel/MainPageViewModel.cs
@@ -1,49 +1,15 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using HealthCareApp.DAL;
+﻿using HealthCareApp.DAL;
 using HealthCareApp.model;
 
 namespace HealthCareApp.viewmodel
-{S
-	public class MainPageViewModel : INotifyPropertyChanged
-	{
-		private ObservableCollection<Patient> _patients;
-		public ObservableCollection<Patient> Patients
-		{
-			get => _patients;
-			set
-			{
-				_patients = value;
-				OnPropertyChanged(nameof(Patients));
-			}
-		}
-
-		public MainPageViewModel()
-		{
-			Patients = new ObservableCollection<Patient>();
-			//RefreshPatientsCommand = new RelayCommand(RefreshPatientList);
-			PopulatePatients();
-		}
+{
+    public class MainPageViewModel
+    {
+	    public List<Patient> Patients { get; set; }
 
 		public void PopulatePatients()
-		{
-			var patients = PatientDal.GetAllPatients();
-			Patients.Clear();
-			foreach (var patient in patients)
-			{
-				Patients.Add(patient);
-			}
-		}
-
-		private void RefreshPatientList(object obj)
-		{
-			PopulatePatients();
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected virtual void OnPropertyChanged(string propertyName)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-	}
+	    {
+		    Patients = PatientDal.GetAllPatients();
+	    }
+    }
 }
